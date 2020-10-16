acbfd16595277235a639cc8c954ad17fc387e9f6
Romuald <Romuald_auc@hotmail.fr>
2020-10-16 09:44:02 +0200

    [Add] Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T Instance { get; private set; }
+
+    [SerializeField] private bool m_bDestroyOnLoad = true;
+
+    protected void Awake()
+    {
+        if (Instance)
+        {
+            Debug.LogError($"[Singleton] Instance de type {typeof(T)} à une seconde instance");
+            return;
+        }
+
+        Instance = (T) this;
+
+        if (!m_bDestroyOnLoad)
+        {
+            DontDestroyOnLoad(Instance);
+        }
+    }
+}
