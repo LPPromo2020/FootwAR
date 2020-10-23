47eaccd63c560beafffa48d253842db656cac1e8
yannisquerenet <71667241+yannisquerenet@users.noreply.github.com>
2020-10-23 11:02:39 +0200

    change variable name
    set variable names as conventional + add comment

--- Assets/Scripts/groundInitializer/groundInitializer.cs
+++ Assets/Scripts/groundInitializer/groundInitializer.cs
@@ -2,26 +2,23 @@
  * Nom de l’auteur : Yannis Querenet
  * Principe du script : Le script sert à initialiser un sol en réalité augmentée
  * Utilisation : Le script doit être lancé après le démarrage d'une partie pour définir un terrain.
+ * A mettre dans le ARSessionOrigin :
+ * - "goPlanePrefab" correspond au prefab "groundVisualizer", 
+ * - "mGroundColor" correspond au material utilisé.
 */
 
 
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 public class groundInitializer : MonoBehaviour
 {
-    public ARRaycastManager m_rmRaycastManager;
-    public ARPlaneManager m_pmPlaneManager;
-    public List<ARRaycastHit> m_lARRayCastHit;
-    public GameObject planeprefab;
-    public const float CUBE_X = 3f;
-    public const float CUBE_Y = 0.01f;
-    public const float CUBE_Z = 5f;
-    public Text debug;
-    public Material green;
+    public ARRaycastManager m_rmRaycastManager; // Le Raycast Manager
+    public ARPlaneManager m_pmPlaneManager; // Le Plane Manager
+    public List<ARRaycastHit> m_lARRayCastHit; // Liste de point trouvés par le raycastmanager
+    public GameObject m_goPlanePrefab; // Prefab du plan
+    public Material m_mGroundColor; // Materiel de couleur du terrain
 
     // Start is called before the first frame update
     void Start()
@@ -41,27 +38,26 @@ public class groundInitializer : MonoBehaviour
             {
                 CreateGround();
                 HidePlaneManager();
-
             }
         }
 
     }
-    void HidePlaneManager()
+    void HidePlaneManager() // Fonction cache les plans détectés, appelée après avoir posé le terrain.
     {
-        foreach (ARPlane arplane in GameObject.FindObjectsOfType<ARPlane>())
+        foreach (ARPlane arPlane in GameObject.FindObjectsOfType<ARPlane>())
         {
-            arplane.GetComponent<MeshRenderer>().material.color = (new Color(0, 0, 0, 0));
+            arPlane.GetComponent<MeshRenderer>().material.color = (new Color(0, 0, 0, 0));
         }
-        planeprefab.GetComponent<MeshRenderer>().material.color = (new Color(0, 0, 0, 0));
+        m_goPlanePrefab.GetComponent<MeshRenderer>().material.color = (new Color(0, 0, 0, 0));
 
     }
 
-    void CreateGround()
+    void CreateGround() // Fonction qui permet de créer le terrain.
     {
         GameObject gameGround = GameObject.CreatePrimitive(PrimitiveType.Cube);
         gameGround.transform.position = m_lARRayCastHit[0].pose.position;
-        gameGround.transform.localScale = new Vector3(CUBE_X + (Random.value * 2), CUBE_Y, CUBE_Z + (Random.value * 2));
-        gameGround.GetComponent<MeshRenderer>().material = green;
+        gameGround.transform.localScale = new Vector3(3f + (Random.value * 2), 0.01f, 5f + (Random.value * 2));
+        gameGround.GetComponent<MeshRenderer>().material = m_mGroundColor;
 
     }
 }
