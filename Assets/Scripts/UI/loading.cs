dec15b8df01fec49075f621259c68a7a5f5b0a49
Dorian <VIDAL>
2020-11-19 16:44:50 +0100

    [ADD] LeanTween

---
+++ Assets/Scripts/UI/loading.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loading : LeanTween
+{
+    public Sprite Round;
+
+    private void Update()
+    {
+        //Round.
+    }
+}
