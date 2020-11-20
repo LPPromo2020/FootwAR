dfcc3d118d920cc1190e1ff5f43b27695147b001
Dorian <VIDAL>
2020-11-20 11:08:40 +0100

    [ADD] Audio manager

---
+++ Assets/Scripts/MANAGER/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/* Auteur : Dorian
+    Permet de controler tout les sons du jeu
+*/
+
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public AudioSource m_asMainMenu;
+    [SerializeField]
+    private AudioClip m_audioClipMusicToPlay;
+    private void Start()
+    {
+        m_asMainMenu = GetComponent<AudioSource>();
+        m_asMainMenu.Play();
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "InGameScene" )
+        {
+            //m_asMainMenu.clip = m_audioClipMusicToPlay;
+            m_asMainMenu.Stop();
+        }
+    }
+}
