f7e45489a3855993cb6ea7d912bd01ee7a2e4865
Romuald <Romuald_auc@hotmail.fr>
2020-11-22 01:39:08 +0100

    [Add] Notifications

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] private List<GameObject> m_listPrefabsLoader;
+
+    void Start()
+    {
+        m_listPrefabsLoader.ForEach(el => Instantiate(el));
+        m_listPrefabsLoader.Clear();
+        m_listPrefabsLoader = null; // (Si fonctionne) Dit au C# de détruire la listes de prefabs
+
+        SceneLoader.LoadScene("Connexion");
+    }
+
+ }
