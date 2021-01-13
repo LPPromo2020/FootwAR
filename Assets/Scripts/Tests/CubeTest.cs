cd71facbe602801f8bdd20b2f76fd5d7a6168771
artifroid42 <51749201+artifroid42@users.noreply.github.com>
2021-01-13 16:13:46 +0100

    Add AR Setup

---
+++ Assets/Scripts/Tests/CubeTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeTest : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject debugPanelGO = GameObject.Find("DebugPanel");
+
+        DebugPanel debugPanel = debugPanelGO.GetComponent<DebugPanel>();
+        debugPanel.DebugText.text = "CUBE CREE " + debugPanel.cubeNB;
+        
+    }
+}
