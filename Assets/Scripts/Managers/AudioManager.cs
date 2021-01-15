e4dd3d43341b83152cd4fd9288a883f9f3139715
Dorian <VIDAL>
2021-01-15 16:24:37 +0100

    [UPDATE +ADD] Song Added + audio manager update

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -9,20 +9,34 @@ using UnityEngine.SceneManagement;
 
 public class AudioManager : Singleton<AudioManager>
 {
+    public float volume = 0.5f;
+    public List<AudioClip> m_audioClipsList;
     public AudioSource m_asMainMenu;
     [SerializeField]
     private AudioClip m_audioClipMusicToPlay;
+    [SerializeField]
+    private AudioClip m_audioClipMusicInGame;
+    private bool m_boolIsPlaying;
+
     private void Start()
     {
         m_asMainMenu = GetComponent<AudioSource>();
-        m_asMainMenu.Play();
+        m_asMainMenu.PlayOneShot(m_audioClipMusicToPlay);
     }
 
     private void Update()
     {
-        if (SceneManager.GetActiveScene().name == "InGameScene" )
+        if (m_asMainMenu.isPlaying == false)
         {
-            m_asMainMenu.Stop();
+            if (SceneManager.GetActiveScene().name == "InGameScene")
+            {
+                m_asMainMenu.PlayOneShot(m_audioClipMusicToPlay);
+            }
+            else
+            {
+                m_asMainMenu.PlayOneShot(m_audioClipMusicToPlay);
+            }
+
         }
     }
 }
