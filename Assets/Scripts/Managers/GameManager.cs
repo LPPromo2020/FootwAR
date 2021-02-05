a40706a07e3a743fb2ff1db1c2a603e5dbe3bbf9
Dorian <VIDAL>
2021-02-05 15:28:01 +0100

    [UPDATE] Comm update

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+//Dorian et Romuald
+//Instancie l'ensemble des managers necessaires à l'initialisation du jeu
+
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private List<GameObject> m_listPrefabsLoader;
